de83d357dc3b6cbe2c5d6c6a8ff218f2704866d4
ram4ik <ram4ik@hotmail.com>
2023-11-03 21:11:11 +0200

    Initial Commit

---
+++ CustomOperatorsInSwiftUI/CustomOperatorsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomOperatorsInSwiftUIApp.swift
+//  CustomOperatorsInSwiftUI
+//
+//  Created by admin on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomOperatorsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
