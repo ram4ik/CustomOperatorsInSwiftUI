d23276e746235135b38cb25355f7b3a254e586a8
ram4ik <ram4ik@hotmail.com>
2023-11-03 21:36:01 +0200

    custom operators

--- CustomOperatorsInSwiftUI/ContentView.swift
+++ CustomOperatorsInSwiftUI/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value: Double = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Custom Operator")
+            .onAppear() {
+                value = 5 +/ 5
+                value = 5 ++/ 5
+                value = 5 ^^^ 5
+            }
+    }
+}
+
+infix operator +/
+infix operator ++/
+infix operator ^^^
+
+extension FloatingPoint {
+    
+    static func +/ (lhs: Self, rhs: Self) -> Self {
+        (lhs + rhs) / 2
+    }
+    
+    static func ++/ (lhs: Self, rhs: Self) -> Self {
+        (lhs + lhs) / rhs
+    }
+    
+    static func ^^^ (lhs: Self, rhs: Self) -> Self {
+        (lhs + lhs) * rhs
     }
 }
 
